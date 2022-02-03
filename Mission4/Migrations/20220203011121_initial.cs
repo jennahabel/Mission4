771040fcec4580a3a7e729ebeaf9581ce053f240
jennahabel <86261801+jennahabel@users.noreply.github.com>
2022-02-02 21:13:24 -0700

    Mission5

---
+++ Mission4/Migrations/20220203011121_initial.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission4.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<string>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    Lent = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies", x => x.MovieId);
+                    table.ForeignKey(
+                        name: "FK_movies_categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 1, "Action/Adventure" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 2, "Comedy" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 3, "Drama" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 4, "Family" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 5, "Horror/Suspense" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 6, "Miscellaneous" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 7, "Television" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 8, "VHS" });
+
+            migrationBuilder.InsertData(
+                table: "movies",
+                columns: new[] { "MovieId", "CategoryId", "Director", "Edited", "Lent", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, 1, "Jon Watts", false, "", "Best Movie", "PG-13", "Spider-Man: No Way Home", "2021" });
+
+            migrationBuilder.InsertData(
+                table: "movies",
+                columns: new[] { "MovieId", "CategoryId", "Director", "Edited", "Lent", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, 2, "Brian Helgeland", false, "", "Classic", "PG-13", "A Knights Tale", "2001" });
+
+            migrationBuilder.InsertData(
+                table: "movies",
+                columns: new[] { "MovieId", "CategoryId", "Director", "Edited", "Lent", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, 7, "Jason Sudeikis Bill, Lawrence Brendan, Hunt Joe Kelly", false, "", "Best TV Show", "PG-13", "Ted Lasso", "2020" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_movies_CategoryId",
+                table: "movies",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "movies");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
